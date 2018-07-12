b37351f466254ed2d9b85ad319999516b7fbbd6d
AntonKlimantow <klimantow@yandex.ru>
2018-07-12 15:57:16 +0300

    Final

--- docs/js/main.js
+++ docs/js/main.js
@@ -27,6 +27,14 @@ $(document).ready(function() {
 	    }
 	});
 
+    var w = $(window).width();
+    if (w < 992) {
+    	$('nav.navigation a').on("click", function(){
+    		menu.slideToggle();
+    	})
+    }
+	
+
 
 	// вызов слайдера
 	$("#top-slider").owlCarousel({
@@ -36,6 +44,12 @@ $(document).ready(function() {
 		navigationText: [],
 		slideSpeed: 1000
 	});
+
+	//slide2id
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+		highlightSelector:"nav a"
+	});
+
 
 });
 
